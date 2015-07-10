33b060f6c2c53f8a229693791c69898d1b02a195
Imxset21 <pr273@cornell.edu>
2015-07-10 02:58:00 -0400

    - Changed add/sub functions to use zipped map iterators - Changed generic type syntax to use "where" clauses where possible - Added array order enum for differentiating between row/column vectors - Added memory use test

--- src/array.rs
+++ src/array.rs
@@ -1,81 +1,109 @@
-/// Array type
+// Array type
 
 use std::ops::Add;
 use std::ops::Sub;
 use std::cmp::PartialEq;
 
+/// Enumerator for whether the vector is horizontal or vertical
+#[derive(Debug, PartialEq)]
+#[allow(dead_code)]
+pub enum Order { Column, Row}
+
 #[derive(Debug)]
-/// Wraps an array in a box
-pub struct Array<T: Add + Sub + Copy + PartialEq>
+/// An array is a boxed vector with additional information (e.g. stride).
+/// Arrays support generics as long as they meet the following criteria:
+/// 1. 
+pub struct Array<T> where T: Add + Sub + Copy + PartialEq
 {
-    my_vec: Box<Vec<T>>,
+    my_vec: Vec<T>,
+    order: Order,
 }
 
-impl<T: Add + Copy + Sub + PartialEq> Array<T>
+impl<T> Array<T> where T: Add + Sub + Copy + PartialEq
 {
     /// A public constructor
-    pub fn new(contents: Vec<T>) -> Array<T>
+    pub fn new(contents: Vec<T>, order: Order) -> Array<T>
     {
         Array
         {
             // I guess this is a copy constructor?
-            my_vec: Box::new(contents),
+            my_vec: contents,
+            order: order
         }
     }
 }
 
-impl<T: Add<Output=T> + Sub + Copy + PartialEq> Add for Array<T>
+/// Implementation of the Add trait
+impl<T> Add for Array<T> where T: Add<Output=T> + Sub + Copy + PartialEq
 {
     type Output = Array<T>;
 
     fn add(self, other: Array<T>) -> Array<T>
     {
-        if self.my_vec.len() != other.my_vec.len()
+        if self.order != other.order
         {
+            panic!("Cannot add two Arrays of different order.");
+        } else if self.my_vec.len() != other.my_vec.len() {
             panic!("Cannot add two Arrays of different sizes.");
         } else if self.my_vec.len() == 0 {
-            return Array { my_vec: Box::<Vec<T>>::new(vec![]) }
+            return Array { my_vec: vec![], order: self.order }
         }
-        
-        let mut b = Vec::with_capacity(self.my_vec.len());
-        for (num1, num2) in self.my_vec.iter().zip(other.my_vec.iter())
+
+        fn add_two<'a, T>((a, b) : (&'a T, &'a T),) -> T
+            where T: Add<Output=T> + Sub + Copy + PartialEq
         {
-            b.push(*num1 + *num2);
+            *a + *b
         }
-        
-        return Array { my_vec : Box::<Vec<T>>::new(b)}
+
+        // Make a zipped iterator containing (vec1[i], vec2[i]), then produce
+        // a map iterator which yields their sum, and collect into a new vec.
+        return Array { my_vec : self.my_vec.iter().zip(other.my_vec.iter())
+                       .map(add_two::<T>).collect::<Vec<T>>(),
+                       order: self.order
+        }
     }
 }
 
-impl<T: Add + Sub<Output=T> + Copy + PartialEq> Sub for Array<T>
+/// Implementation of the Subtract trait
+impl<T> Sub for Array<T> where T: Add + Sub<Output=T> + Copy + PartialEq
 {
     type Output = Array<T>;
 
     fn sub(self, other: Array<T>) -> Array<T>
     {
-        if self.my_vec.len() != other.my_vec.len()
+        if self.order != other.order
         {
+            panic!("Cannot add two Arrays of different order.");
+        } else if self.my_vec.len() != other.my_vec.len() {
             panic!("Cannot add two Arrays of different sizes.");
         } else if self.my_vec.len() == 0 {
-            return Array { my_vec: Box::<Vec<T>>::new(vec![]) }
+            return Array { my_vec: vec![], order: self.order }
         }
-        
-        let mut b = Vec::with_capacity(self.my_vec.len());
-        for (num1, num2) in self.my_vec.iter().zip(other.my_vec.iter())
+
+        fn sub_two<'a, T>((a, b) : (&'a T, &'a T),) -> T
+            where T: Add + Sub<Output=T> + Copy + PartialEq
         {
-            b.push(*num1 - *num2);
+            *a - *b
         }
         
-        return Array { my_vec : Box::<Vec<T>>::new(b)}
+        // Make a zipped iterator containing (vec1[i], vec2[i]), then produce
+        // a map iterator which yields their sub, and collect into a new vec.
+        return Array { my_vec : self.my_vec.iter().zip(other.my_vec.iter())
+                       .map(sub_two::<T>).collect::<Vec<T>>(),
+                       order: self.order
+        }
     }
 }
 
-impl<T: Add + Sub + Copy + PartialEq> PartialEq for Array<T>
+/// Implementation of the PartialEq trait
+impl<T> PartialEq for Array<T> where T: Add + Sub + Copy + PartialEq
 {
     fn eq(&self, other: &Array<T>) -> bool
     {
-        if self.my_vec.len() != other.my_vec.len()
+        if self.order != other.order
         {
+            panic!("Cannot add two Arrays of different order.");
+        } else if self.my_vec.len() != other.my_vec.len() {
             panic!("Cannot compare arrays of different dimensions.")
         }
         
--- src/lib.rs
+++ src/lib.rs
@@ -4,31 +4,39 @@ mod array;
 mod test
 {
     use array;
+    use std::mem;
 
     #[test]
     fn test_eq()
     {
-        let a = array::Array::<i32>::new(vec![1, 2, 3]);
-        let b = array::Array::<i32>::new(vec![1, 2, 3]);
+        let a = array::Array::<i32>::new(vec![1, 2, 3], array::Order::Row);
+        let b = array::Array::<i32>::new(vec![1, 2, 3], array::Order::Row);
         assert!(a == b);
     }
     
     #[test]
     fn test_sub()
     {
-        let a = array::Array::<i32>::new(vec![1, 2, 3]);
-        let b = array::Array::<i32>::new(vec![4, 5, 6]);
-        let c = array::Array::<i32>::new(vec![3, 3, 3]);
+        let a = array::Array::<i32>::new(vec![1, 2, 3], array::Order::Row);
+        let b = array::Array::<i32>::new(vec![4, 5, 6], array::Order::Row);
+        let c = array::Array::<i32>::new(vec![3, 3, 3], array::Order::Row);
         assert!(b - a == c);
     }
 
     #[test]
     fn test_add()
     {
-        let a = array::Array::<i32>::new(vec![1, 2, 3]);
-        let b = array::Array::<i32>::new(vec![4, 5, 6]);
-        let c = array::Array::<i32>::new(vec![5, 7, 9]);
+        let a = array::Array::<i32>::new(vec![1, 2, 3], array::Order::Row);
+        let b = array::Array::<i32>::new(vec![4, 5, 6], array::Order::Row);
+        let c = array::Array::<i32>::new(vec![5, 7, 9], array::Order::Row);
         assert!(a + b == c);
     }
 
+    #[test]
+    fn test_memuse()
+    {
+        let a = array::Array::<i32>::new(vec![0; 100], array::Order::Row);
+        let b = array::Array::<i32>::new(vec![4, 5, 6], array::Order::Row);
+        assert!(mem::size_of_val(&a) == mem::size_of_val(&b));
+    }
 }
