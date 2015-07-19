1b857a64f42ca189fcb1b0e2620b2fcafb6b607f
Pedro Rittner <pedro.rittner@outlook.com>
2015-07-18 22:41:52 -0400

    - Added Travis build script, OpenBLAS functions

--- src/lib.rs
+++ src/lib.rs
@@ -242,11 +242,66 @@ mod test
     #[test]
     fn test_openblas_ddot()
     {
-        let a1 = arr![1., 2., 3.];
-        let a2 = arr![4., 5., 6.];
+        let a1 = arr![1f64, 2f64, 3f64];
+        let a2 = arr![4f64, 5f64, 6f64];
         let blas_result = openblas::openblas_ddot(&a1, &a2);
         let gen_result = a1 * a2;  // Generic dot product
         assert_eq!(gen_result, blas_result);
     }
+
+    #[test]
+    fn test_openblas_sdot()
+    {
+        let a1 = arr![1f32, 2f32, 3f32];
+        let a2 = arr![4f32, 5f32, 6f32];
+        let blas_result = openblas::openblas_sdot(&a1, &a2);
+        let gen_result = a1 * a2;  // Generic dot product
+        assert_eq!(gen_result, blas_result);
+    }
+
+    #[test]
+    fn test_openblas_sasum()
+    {
+        let a1 = arr![1f32, 2f32, 3f32];
+        let elem_sum = 1f32 + 2f32 + 3f32;
+        let blas_result = openblas::openblas_sasum(&a1);
+        assert_eq!(elem_sum, blas_result);
+    }
+
+    #[test]
+    fn test_openblas_dasum()
+    {
+        let a1 = arr![1f64, 2f64, 3f64];
+        let elem_sum = 1f64 + 2f64 + 3f64;
+        let blas_result = openblas::openblas_dasum(&a1);
+        assert_eq!(elem_sum, blas_result);
+    }
+
+    #[test]
+    fn test_openblas_daxpy()
+    {
+        let a1 = arr![1f64, 2f64, 3f64];
+        let mut a2 = arr![0f64, 0f64, 0f64];
+        let alpha = 5f64;
+        
+        openblas::openblas_daxpy(&a1, alpha, &mut a2);
+        
+        let result_arr = arr![5f64, 10f64, 15f64];
+        assert_eq!(result_arr, a2);
+    }
+
+    #[test]
+    fn test_openblas_saxpy()
+    {
+        let a1 = arr![1f32, 2f32, 3f32];
+        let mut a2 = arr![1f32, 2f32, 3f32];
+        let alpha = 5f32;
+        
+        openblas::openblas_saxpy(&a1, alpha, &mut a2);
+        
+        let result_arr = arr![6f32, 12f32, 18f32];
+        assert_eq!(result_arr, a2);
+    }
 }
+
 
--- src/openblas.rs
+++ src/openblas.rs
@@ -1,9 +1,9 @@
-// #![feature(libc)]   // <- Uncomment for static analysis
+// #![feature(libc)]  // <- Uncomment for static analysis
 // extern crate libc; // <- Uncomment for static analysis
-// mod array;  // <- Uncomment for static analysis
+// mod array;         // <- Uncomment for static analysis
 
 use array::Array;
-use libc::{c_int, c_double};
+use libc::{c_int, c_double, c_float};
 
 #[link(name = "blas")]
 extern
@@ -12,9 +12,30 @@ extern
     fn cblas_ddot (N: c_int,
                    X: *const c_double, incX: c_int,
                    Y: *const c_double, incY: c_int) -> c_double;
+
+    /// Computes a vector-vector dot product for f32 ("single") precision.
+    fn cblas_sdot (N: c_int,
+                   X: *const c_float, incX: c_int,
+                   Y: *const c_float, incY: c_int) -> c_float;
+
+    /// Computes the sum of magnitudes of the vector elements, single precision
+    fn cblas_sasum (N: c_int, X: *const c_float, incX: c_int) -> c_float;
+    
+    /// Computes the sum of magnitudes of the vector elements, single precision
+    fn cblas_dasum (N: c_int, X: *const c_double, incX: c_int) -> c_double;
+
+    /// Computes a vector-scalar product and adds the result to a vector (f32)
+    fn cblas_saxpy(n: c_int,
+                   alpha: c_float, X: *const c_float, incX: c_int,
+                   Y: *mut c_float, incY: c_int);
+
+    /// Computes a vector-scalar product and adds the result to a vector (f64)
+    fn cblas_daxpy(n: c_int,
+                   alpha: c_double, X: *const c_double, incX: c_int,
+                   Y: *mut c_double, incY: c_int);
 }
 
-/// OpenBLAS implementation of the dot product of two vectors, double precision
+/// OpenBLAS computation of the dot product of two vectors, double precision
 pub fn openblas_ddot(arr_a: &Array<f64>, arr_b: &Array<f64>) -> f64
 {
     if arr_a.len() != arr_b.len()
@@ -30,5 +51,92 @@ pub fn openblas_ddot(arr_a: &Array<f64>, arr_b: &Array<f64>) -> f64
         cblas_ddot (n,
                     arr_a.as_ptr(), inc_x,
                     arr_b.as_ptr(), inc_y)
+    }
+}
+
+/// OpenBLAS computation of the dot product of two vectors, single precision
+pub fn openblas_sdot(arr_a: &Array<f32>, arr_b: &Array<f32>) -> f32
+{
+    if arr_a.len() != arr_b.len()
+    {
+        panic!("Dot product of vectors of different lengths is undefined.")
+    }
+
+    let n : c_int = arr_a.len() as c_int;
+    let inc_x : c_int = 1 as c_int;
+    let inc_y : c_int = 1 as c_int;
+    
+    unsafe {
+        cblas_sdot(n,
+                   arr_a.as_ptr(), inc_x,
+                   arr_b.as_ptr(), inc_y)
+    }
+}
+
+/// OpenBLAS computation of sum of magnitudes of the vector elements (f32)
+pub fn openblas_sasum (arr_a: &Array<f32>) -> f32
+{
+    let n : c_int = arr_a.len() as c_int;
+    let inc_x : c_int = 1 as c_int;
+    
+    unsafe {
+        cblas_sasum(n, arr_a.as_ptr(), inc_x)
+    }
+}
+
+/// OpenBLAS computation of sum of magnitudes of the vector elements (f64)
+pub fn openblas_dasum (arr_a: &Array<f64>) -> f64
+{
+    let n : c_int = arr_a.len() as c_int;
+    let inc_x : c_int = 1 as c_int;
+    
+    unsafe {
+        cblas_dasum(n, arr_a.as_ptr(), inc_x)
+    }
+}
+
+/// OpenBLAS computation of vector-scalar product, adds the result to a vector (f64).
+pub fn openblas_daxpy(arr_x: &Array<f64>,
+                      alpha: f64,
+                      arr_y: &mut Array<f64>)
+{
+    if arr_x.len() != arr_y.len()
+    {
+        panic!("Vector addition for vectors of different lengths is undefined.")
+    }
+
+    let n : c_int = arr_x.len() as c_int;
+    let inc_x : c_int = 1 as c_int;
+    let inc_y : c_int = 1 as c_int;
+
+    unsafe
+    {
+        cblas_daxpy(n,
+                    alpha as c_double,
+                    arr_x.as_ptr(), inc_x,
+                    arr_y.as_mut_ptr(), inc_y);
+    }
+}
+
+/// OpenBLAS computation of vector-scalar product, adds the result to a vector (f32)
+pub fn openblas_saxpy(arr_x: &Array<f32>,
+                      alpha: f32,
+                      arr_y: &mut Array<f32>)
+{
+    if arr_x.len() != arr_y.len()
+    {
+        panic!("Vector addition for vectors of different lengths is undefined.")
+    }
+
+    let n : c_int = arr_x.len() as c_int;
+    let inc_x : c_int = 1 as c_int;
+    let inc_y : c_int = 1 as c_int;
+
+    unsafe
+    {
+        cblas_saxpy(n,
+                    alpha as c_float,
+                    arr_x.as_ptr(), inc_x,
+                    arr_y.as_mut_ptr(), inc_y);
     }
 }
